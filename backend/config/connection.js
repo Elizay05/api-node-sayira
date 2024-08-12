c9a4c03048b60edfae4ef71c9f98129539748a50
Elizay05 <sayiis2005@gmail.com>
2024-08-12 06:57:11 -0500

    Configurando el connection.js

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const URI = `mongodb+srv://${process.env.USERBD}:${process.env.PASSWORDBD}@adso2669736.cdhps8h.mongodb.net/${process.env.BD}?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://${process.env.USERBD}:${process.env.PASSWORDBD}@adso2669736.kxt4qr6.mongodb.net/${process.env.BD}?retryWrites=true&w=majority`;
 
 mongoose.connect(URI);
 
